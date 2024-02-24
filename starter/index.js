de7e896710086d3d6bd9dd3963ae752baf32ba14
Amr Shehata <amr_shehata@hotmail.com>
2024-02-24 17:26:57 +0000

    Added and tested all codes ,html and css styles

--- starter/index.js
+++ starter/index.js
@@ -4,14 +4,10 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./src/page-template.js");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./src/page-template.js");
-
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 const team = [];
 
@@ -53,20 +49,118 @@ const promptEmployee = async () => {
             choices: ["Add an Engineer", "Add an Intern", "Finish building the team"]
         }
     ]);
-    if (userChoice === "Engineer") {
+    if (userChoice === "Add an Engineer") {
         await promptEngineer();
-    } else if (userChoice === "Intern") {
+    } else if (userChoice === "Add an Intern") {
         await promptIntern();
-    } else {
-        generateHTML();
     }
 };
 
-// init() function is responsible for initializing the program
+const promptEngineer = async () => {
+    const { name, id, email, github } = await inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter engineer's name:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Enter engineer's ID:",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter engineer's email:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Enter engineer's GitHub username:",
+            name: "github"
+        }
+    ]);
+    const engineer = new Engineer(name, id, email, github);
+    team.push(engineer);
+    console.log("Engineer added to the team:", engineer);
+    console.log("Current team:", team);
+};
+
+const promptIntern = async () => {
+    const { name, id, email, school } = await inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter intern's name:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Enter intern's ID:",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter intern's email:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Enter intern's school:",
+            name: "school"
+        }
+    ]);
+    const intern = new Intern(name, id, email, school);
+    team.push(intern);
+    console.log("Intern added to the team:", intern);
+    console.log("Current team:", team);
+};
+
+const generateHTML = () => {
+    console.log("Output directory path:", OUTPUT_DIR);
+    console.log("Checking if output directory exists...");
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+        console.log("Output directory created at", OUTPUT_DIR);
+    } else {
+        console.log("Output directory already exists at", OUTPUT_DIR);
+    }
+
+    console.log("Writing HTML file to path:", outputPath);
+    console.log("Attempting to write HTML file...");
+
+    // Generate HTML for the entire team
+    const html = render(team);
+
+    // Write HTML content to file
+    fs.writeFileSync(outputPath, html, "utf-8");
+
+    console.log("Team HTML generated at", outputPath);
+    console.log("Checking if HTML file exists after write attempt:", fs.existsSync(outputPath));
+
+    console.log("Contents of output directory:", fs.readdirSync(OUTPUT_DIR));
+};
+
 const init = async () => {
     console.log("Please enter manager details:");
     await promptManager();
-    await promptEmployee();
+    
+    let finishBuilding = false;
+    while (!finishBuilding) {
+        await promptEmployee();
+        const { userChoice } = await inquirer.prompt([
+            {
+                type: "list",
+                message: "Would you like to add another team member or finish building the team?",
+                name: "userChoice",
+                choices: ["Add another team member", "Finish building the team"]
+            }
+        ]);
+        if (userChoice === "Finish building the team") {
+            finishBuilding = true;
+        }
+    }
+
+    generateHTML(); // Call generateHTML() after the user has finished building the team
 };
 
 init();
+
