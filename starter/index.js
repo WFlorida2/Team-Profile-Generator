a329aab650e2ab1c389521f8fae5a324c2ac79be
Amr Shehata <amr_shehata@hotmail.com>
2024-02-24 13:00:12 +0000

    added and tested a Manager prompt

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,35 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const team = [];
+
+const promptManager = async () => {
+    const { name, id, email, officeNumber } = await inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter manager's name:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Enter manager's ID:",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter manager's email:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Enter manager's office number:",
+            name: "officeNumber"
+        }
+    ]);
+    const manager = new Manager(name, id, email, officeNumber);
+    team.push(manager);
+    console.log("Manager added to the team:", manager);
+    console.log("Current team:", team);
+};
+// Call the function to start prompting for manager information
+promptManager();
